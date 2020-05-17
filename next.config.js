0e2ea9ecd0084e5b7ac57ac15c1ed3d964684352
NguyenPham <gsphamnguyen@gmail.com>
2020-05-17 08:55:46 +0700

    Update next conf

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  experimental: {
-    basePath: isProd ? '' : '/tailwind-starter-kit',
-  },
+  basePath: isProd ? '' : '/',
+  assetPrefix: isProd ? '' : '/'
 }
